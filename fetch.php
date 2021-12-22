1fc1c9d3925a5852a45ec79e7ea175bcc81dbaa0
Roderic Page <rdmpage@gmail.com>
2021-12-22 10:54:58 +0000

    More pics

--- fetch.php
+++ fetch.php
@@ -4221,7 +4221,7 @@ $names=array('Actinobacteria','Streptomycetaceae','Microbacteriaceae', 'Actinomy
 
 $names=array('Hypochilidae');
 
-$names=array('Erebidae');
+$names=array('Hydrochidae','Melolonthidae','Agyrtidae');
 
 //----------------------------------------------------------------------------------------
 function get_image_filename($base_dir, $name, $extension = 'svg')
