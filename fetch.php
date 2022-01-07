11cff288cc0e05eb4e3f678b22f8f9bf8593fe17
Roderic Page <rdmpage@gmail.com>
2022-01-07 10:03:59 +0000

    More pics

--- fetch.php
+++ fetch.php
@@ -4225,7 +4225,7 @@ $names=array('Thomisidae','Scytodidae','Symphytognathidae','Tetragnathidae','Tho
 
 $names=array('Atyidae','Cambaridae','Diogenidae','Gecarcinucidae','Homolodromiidae','Palaemonidae','Potamonautidae');
 
-$names=array('Clitellata');
+$names=array('Chthoniidae','Neobisiidae');
 
 //----------------------------------------------------------------------------------------
 function get_image_filename($base_dir, $name, $extension = 'svg')
