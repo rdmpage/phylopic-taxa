1acfb4cfb2c9911fdb8887968b412ecaca179055
Roderic Page <rdmpage@gmail.com>
2022-01-10 17:48:01 +0000

    More pics

--- fetch.php
+++ fetch.php
@@ -4227,7 +4227,8 @@ $names=array('Atyidae','Cambaridae','Diogenidae','Gecarcinucidae','Homolodromiid
 
 $names=array('Agelenidae','Araneidae','Archaeidae','Austrochilidae','Clubionidae','Corinnidae','Deinopidae','Dysderidae','Gradungulidae','Hypochilidae','Idiopidae','Linyphiidae','Liocranidae','Nesticidae','Ochyroceratidae','Oonopidae','Palpimanidae','Pisauridae','Salticidae','Scytodidae','Symphytognathidae','Telemidae','Tetragnathidae','Theraphosidae','Thomisidae');
 
-$names=array('Ariophantidae','Camaenidae','Cepolidae','Clausiliidae','Streptaxidae');
+$names=array('Cunaxidae','Eriophyidae','Tenuipalpidae','Tetranychidae','Tydeidae');
+
 
 //----------------------------------------------------------------------------------------
 function get_image_filename($base_dir, $name, $extension = 'svg')
